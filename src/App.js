c8c6b25e5c0adee44969b22eb16b074e06190619
zadremal <anton.kamynin@gmail.com>
2018-05-06 13:00:00 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./App.css";
+import Mainscreen from "./Mainscreen";
+
+class App extends Component {
+  render() {
+    return <Mainscreen />;
+  }
+}
+
+export default App;
---
+++ src/Mainscreen/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row center-xs">
+          <div className="col-xs-6">test</div>
+          <div className="col-xs-6">test</div>
+        </div>
+      </div>
+    );
+  }
+}
