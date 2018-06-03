d173a2f93a3660f9febd33177169bfeb23e7d4e6
zadremal <anton.kamynin@gmail.com>
2018-06-03 16:18:53 +0300

    feat: add contacts page

--- src/Contacts/index.js
+++ src/Contacts/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-import { Mainscreen } from "../UI";
+import Section, { Heading } from "../UI/section";
+import { Map, TextBlock, Wrap, Contact } from "./Styled";
+
 class index extends Component {
   state = {
     name: "",
@@ -46,32 +48,26 @@ class index extends Component {
 
   render() {
     return (
-      <Mainscreen>
-        <form action="">
-          <input
-            onInput={this.updateValue}
-            name="name"
-            type="text"
-            placeholder="Ваше имя"
-            value={this.state.name}
-          />
-          <input
-            onInput={this.updateValue}
-            name="phone"
-            type="text"
-            placeholder="Номер телефона"
-            value={this.state.phone}
-          />
-          <input
-            onInput={this.updateValue}
-            name="email"
-            type="text"
-            placeholder="email"
-            value={this.state.email}
-          />
-          <button onClick={this.submitForm}>отправить</button>
-        </form>
-      </Mainscreen>
+      <Section>
+        <Wrap>
+          <TextBlock>
+            <Heading>Контакты</Heading>
+            <Contact>г. Санкт-Петербург, пр-кт Испытателей 26 к2</Contact>
+            <Contact>тел: 8 (812) 981-53-35</Contact>
+            <Contact>retro@pioner-club.com</Contact>
+          </TextBlock>
+          <Map>
+            <iFrame
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1994.520011552468!2d30.265994116099762!3d60.006452681900875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4696343be8afe5d7%3A0x8c0a80070d563725!2z0J_QuNC-0L3QtdGA!5e0!3m2!1sru!2sru!4v1528031541930"
+              width="100%"
+              height="600"
+              frameborder="0"
+              style={{ border: "0" }}
+              allowfullscreen
+            />
+          </Map>
+        </Wrap>
+      </Section>
     );
   }
 }
