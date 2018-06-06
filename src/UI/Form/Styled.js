9c1c045e40deaacf70c7fa879366fa14da391180
zadremal <anton.kamynin@gmail.com>
2018-06-06 12:41:24 +0300

    fix: fix masked input bug on mobile refactor: refactor form for better UI

--- src/UI/Form/Styled.js
+++ src/UI/Form/Styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-import { colorPr, colorAcc, colorDark } from "../index";
-import MaskedInput from "react-maskedinput";
+import { colorPr, colorDark, media } from "../index";
+import InputMask from "react-input-mask";
 
 export const Form = styled.form`
   width: 100%;
@@ -10,12 +10,14 @@ export const Form = styled.form`
 
 export const Heading = styled.h4`
   font-family: Intro, arial, sans-serif;
-  font-size: 1.75rem;
-  margin: 0 auto;
+  font-size: 1rem;
+  max-width: 400px;
+  margin: 1.25rem auto;
   text-align: center;
-  margin-bottom: 2rem;
   color: ${colorDark};
-  transform: all 0.2s ease-out;
+  ${media.md`
+    font-size: 1.5rem;
+  `};
 `;
 
 export const Error = styled.h5`
@@ -23,13 +25,13 @@ export const Error = styled.h5`
   color: ${colorPr};
 `;
 
-export const Input = styled(MaskedInput)`
+export const Input = styled(InputMask)`
   display: block;
   background: transparent;
   position: relative;
   border: none;
   width: 100%;
-  border-bottom: 3px solid ${colorPr};
+  border-bottom: 3px solid ${colorDark};
   padding: 0.5rem 1rem;
   font-size: 1.5rem;
   margin-bottom: 1.75rem;
@@ -38,18 +40,28 @@ export const Input = styled(MaskedInput)`
   font-weight: 700;
   color: ${colorDark};
   transition: all 0.1s linear;
+  text-transform: ${props => props.transform};
   &:focus {
-    box-shadow: 0 4px 2px -2px rgba(235, 31, 6, 0.7);
+    box-shadow: 0 4px 2px -2px rgba(235, 31, 6, 0.8);
+    border-bottom: 3px solid rgba(235, 31, 6, 1);
   }
 `;
 
 export const Label = styled.label`
-  color: ${colorAcc};
-  font-size: 1.25rem;
+  font-size: 1rem;
   font-weight: 700;
+  color: ${colorDark};
 `;
 
 export const Submit = styled.div`
   text-align: center;
   margin-top: 1rem;
+`;
+
+export const Response = styled.div`
+  text-align: center;
+  font-size: 1.25rem;
+  padding: 3em;
+  display: flex;
+  justify-content: center;
 `;
