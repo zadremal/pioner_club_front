8c1a99fe8b96bbc09f739af3c13e4026bb6ad4c4
zadremal <anton.kamynin@gmail.com>
2018-06-06 13:09:35 +0300

    hotfix: add container to contact page

--- src/Contacts/index.js
+++ src/Contacts/index.js
@@ -49,24 +49,30 @@ class index extends Component {
   render() {
     return (
       <Section>
-        <Wrap>
-          <TextBlock>
-            <Heading>Контакты</Heading>
-            <Contact>г. Санкт-Петербург, пр-кт Испытателей 26 к2</Contact>
-            <Contact>тел: 8 (812) 981-53-35</Contact>
-            <Contact>retro@pioner-club.com</Contact>
-          </TextBlock>
-          <Map>
-            <iFrame
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1994.520011552468!2d30.265994116099762!3d60.006452681900875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4696343be8afe5d7%3A0x8c0a80070d563725!2z0J_QuNC-0L3QtdGA!5e0!3m2!1sru!2sru!4v1528031541930"
-              width="100%"
-              height="600"
-              frameborder="0"
-              style={{ border: "0" }}
-              allowfullscreen
-            />
-          </Map>
-        </Wrap>
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12">
+              <Wrap>
+                <TextBlock>
+                  <Heading>Контакты</Heading>
+                  <Contact>г. Санкт-Петербург, пр-кт Испытателей 26 к2</Contact>
+                  <Contact>тел: 8 (812) 981-53-35</Contact>
+                  <Contact>retro@pioner-club.com</Contact>
+                </TextBlock>
+                <Map>
+                  <iFrame
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1994.520011552468!2d30.265994116099762!3d60.006452681900875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4696343be8afe5d7%3A0x8c0a80070d563725!2z0J_QuNC-0L3QtdGA!5e0!3m2!1sru!2sru!4v1528031541930"
+                    width="100%"
+                    height="600"
+                    frameborder="0"
+                    style={{ border: "0" }}
+                    allowfullscreen
+                  />
+                </Map>
+              </Wrap>
+            </div>
+          </div>
+        </div>
       </Section>
     );
   }
