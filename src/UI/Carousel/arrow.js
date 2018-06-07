d30d7125a68e138ca11877faeb4d5da40ec3348d
zadremal <anton.kamynin@gmail.com>
2018-06-07 16:50:44 +0300

    refactor: change carousel arrow style

---
+++ src/UI/Carousel/arrow.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <g>
+      <g id="keyboard-arrow-left">
+        <polygon points="247.35,270.3 130.05,153 247.35,35.7 211.65,0 58.65,153 211.65,306" />
+      </g>
+    </g>
+  );
+};
+
+export default index;
