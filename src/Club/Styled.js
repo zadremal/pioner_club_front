6572a9ff2861d3801ef7b14983f069e4e66da279
zadremal <anton.kamynin@gmail.com>
2018-06-14 17:24:57 +0300

    feat: add fetch util component refactor: better images, code style, file structure in Club component

--- src/Club/Styled.js
+++ src/Club/Styled.js
@@ -1,19 +1,6 @@
 import styled from "styled-components";
-import { colorPr } from "../UI/";
 
 export const Plan = styled.img`
   width: 100%;
   cursor: pointer;
-`;
-
-export const ClubFeature = styled.h3`
-  font-family: Intro, sans-serif;
-  text-align: center;
-  max-width: 300px;
-  border-bottom: 2px solid ${colorPr};
-  margin: 0 auto;
-  font-size: 1rem;
-  font-weight: 300;
-  margin-bottom: 1.5rem;
-  padding-bottom: 1.5rem;
 `;
