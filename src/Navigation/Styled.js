f9a50ff4138e2e1259a9d1ad84f3d5efa5d8d178
zadremal <anton.kamynin@gmail.com>
2018-06-15 00:13:20 +0300

    fix: - remove unused import from navigation; refactor: - optimize 404 image

--- src/Navigation/Styled.js
+++ src/Navigation/Styled.js
@@ -1,4 +1,4 @@
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 import { colorPr, colorDark, colorAcc, media } from "../UI";
 
