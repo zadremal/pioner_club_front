c804b9f2dd3f1717c0da9ffbc53d810c8258a3ed
zadremal <anton.kamynin@gmail.com>
2018-11-09 20:01:24 +0300

    add new menu

--- src/Birthday/index.js
+++ src/Birthday/index.js
@@ -1,26 +1,12 @@
 import React, { Component, Fragment } from "react";
 import Helmet from "react-helmet";
 import ym from "react-yandex-metrika";
-import {
-  LandingMainHeading,
-  HeadingHighlighted,
-  LandingMainscreen,
-  Overlay,
-  MainscreenWrap,
-  Description
-} from "../UI";
+import { LandingMainHeading, HeadingHighlighted, LandingMainscreen, Overlay, MainscreenWrap, Description } from "../UI";
 import { ButtonUpWhite } from "../UI/buttons";
 import Section, { Heading } from "../UI/section";
 import Modal from "../UI/Modal";
 import Form from "../UI/Form/Birthday";
-import {
-  Detail,
-  DealImg,
-  DealDescription,
-  Highlighted,
-  CardWrap,
-  Special
-} from "./Styled";
+import { Detail, DealImg, DealDescription, Highlighted, CardWrap, Special } from "./Styled";
 import Card from "./Card";
 
 import background from "./img/background.jpg";
@@ -73,17 +59,13 @@ class index extends Component {
           <MainscreenWrap>
             <LandingMainHeading>
               У вас скоро день рождения? Подарим Вам <br />
-              <HeadingHighlighted>
-                скидку 50% на любой депозит*
-              </HeadingHighlighted>
+              <HeadingHighlighted>скидку 50% на любой депозит*</HeadingHighlighted>
             </LandingMainHeading>
             <Detail>
-              *выбирайте любой столик, приходите до 00-00, и платите за него в 2
-              раза меньше! (не включает стоимость обслуживания)
+              *выбирайте любой столик, приходите до 00-00, и платите за него в 2 раза меньше! (не включает стоимость
+              обслуживания)
             </Detail>
-            <ButtonUpWhite onClick={this.toggleModal}>
-              Получить скидку
-            </ButtonUpWhite>
+            <ButtonUpWhite onClick={this.toggleModal}>Получить скидку</ButtonUpWhite>
           </MainscreenWrap>
           <Overlay />
         </LandingMainscreen>
@@ -96,22 +78,21 @@ class index extends Component {
               <div className="col-xs-12 col-md-4 center-xs">
                 <DealImg src={percentage} />
                 <DealDescription>
-                  <Highlighted>Скидка 50 % </Highlighted>на все меню бара и
-                  кухни , при оплате депозита за стол.
+                  <Highlighted>Скидка 50 % </Highlighted>
+                  на все меню бара и кухни , при оплате депозита за стол.
                 </DealDescription>
               </div>
               <div className="col-xs-12 col-md-4  center-xs">
                 <DealImg src={fencing} />
                 <DealDescription>
-                  <Highlighted>Бесплатный вход </Highlighted>для именинника и
-                  5-и гостей
+                  <Highlighted>Бесплатный вход </Highlighted>
+                  для именинника и 5-и гостей
                 </DealDescription>
               </div>
               <div className="col-xs-12 col-md-4  center-xs">
                 <DealImg src={trumpet} />
                 <DealDescription>
-                  <Highlighted>Торжественное посвящение </Highlighted>в ряды
-                  Пионеров!
+                  <Highlighted>Торжественное посвящение </Highlighted>в ряды Пионеров!
                 </DealDescription>
               </div>
             </div>
@@ -125,30 +106,16 @@ class index extends Component {
               </div>
               <div className="col-xs-12">
                 <Description>
-                  Ночной клуб "ПИОНЕР" знает толк в проведении праздников.
-                  Особенно, если это День Рождения! Мы рады предложить для
-                  именинников и их друзей отпраздновать свой День Рождения в
-                  уникальной атмосфере XX века, в которой Вы совершите
-                  увлекательное путешествие в дни своей молодости и получите
-                  незабываемые эмоции! Вот что Вас ожидает у нас в клубе:
+                  Ночной клуб "ПИОНЕР" знает толк в проведении праздников. Особенно, если это День Рождения! Мы рады
+                  предложить для именинников и их друзей отпраздновать свой День Рождения в уникальной атмосфере XX
+                  века, в которой Вы совершите увлекательное путешествие в дни своей молодости и получите незабываемые
+                  эмоции! Вот что Вас ожидает у нас в клубе:
                 </Description>
                 <CardWrap>
-                  <Card
-                    background={party}
-                    heading="Только Хиты: лучшая музыка от 70-х до современной"
-                  />
-                  <Card
-                    background={dance}
-                    heading="Культовые выступления персонала"
-                  />
-                  <Card
-                    background={karaoke}
-                    heading="Караоке-бар с фонотекой на 45 000 песен"
-                  />
-                  <Card
-                    background={pioner}
-                    heading="Настоящая пионерская линейка и вынос знамени под гимн СССР"
-                  />
+                  <Card background={party} heading="Только Хиты: лучшая музыка от 70-х до современной" />
+                  <Card background={dance} heading="Культовые выступления персонала" />
+                  <Card background={karaoke} heading="Караоке-бар с фонотекой на 45 000 песен" />
+                  <Card background={pioner} heading="Настоящая пионерская линейка и вынос знамени под гимн СССР" />
                 </CardWrap>
               </div>
             </div>
@@ -161,15 +128,12 @@ class index extends Component {
                 <Special>
                   <Heading contrast children="спецпрограмма для именинников" />
                   <Description contrast>
-                    Каждые выходные для именинников мы устраиваем специальную
-                    церемонию - посвящение Пионеры! Для тех кто успел в свое
-                    время побыть в рядах пионеров - это шанс пережить заново
-                    этот момент. Для остальных - уникальный шанс вступить в
-                    самое масштабное в мире объединение молодежи!
+                    Каждые выходные для именинников мы устраиваем специальную церемонию - посвящение Пионеры! Для тех
+                    кто успел в свое время побыть в рядах пионеров - это шанс пережить заново этот момент. Для остальных
+                    - уникальный шанс вступить в самое масштабное в мире объединение молодежи!
                   </Description>
                   <Description contrast>
-                    Пионерские галстуки, торжественная музыка и присяга на
-                    верность Советскому Союзу прилагаются!
+                    Пионерские галстуки, торжественная музыка и присяга на верность Советскому Союзу прилагаются!
                   </Description>
                 </Special>
               </div>
@@ -184,10 +148,12 @@ class index extends Component {
               </div>
               <div className="col-xs-12 col-md-6">
                 <Description>
-                  Приходите к нам в ближайшие выходные! Наше предложение
-                  действует для всех именинников неделю до, и неделю после дня
-                  рождения! Заполните форму обратной связи, и наш менеджер
-                  свяжется с вами и ответит на все интересущие вопросы
+                  Приходите к нам в ближайшие выходные! Наше предложение действует для всех именинников неделю до, и
+                  неделю после дня рождения! Заполните форму обратной связи, и наш менеджер свяжется с вами и ответит на
+                  все интересущие вопросы
+                  <br />
+                  <br />
+                  Действует при бронировании стола. Предложение по данной акции может быть ограничено.
                 </Description>
               </div>
               <div className="col-xs-12 col-md-6">
